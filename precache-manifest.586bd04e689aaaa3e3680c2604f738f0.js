9dd628028ee3af6e18e91607bd7b9abb1cef7672
saifGit2417 <saifwork2417@gmail.com>
2022-11-08 20:02:18 +0530

    Updates

---
+++ precache-manifest.586bd04e689aaaa3e3680c2604f738f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e97d8fda9100f27bdee",
+    "url": "/TextManipulator/static/css/main.32e27d57.chunk.css"
+  },
+  {
+    "revision": "8e97d8fda9100f27bdee",
+    "url": "/TextManipulator/static/js/main.8e97d8fd.chunk.js"
+  },
+  {
+    "revision": "52077699d4e480884aec",
+    "url": "/TextManipulator/static/js/1.52077699.chunk.js"
+  },
+  {
+    "revision": "7c1add863ba05fe90a43",
+    "url": "/TextManipulator/static/css/2.b1f186f9.chunk.css"
+  },
+  {
+    "revision": "7c1add863ba05fe90a43",
+    "url": "/TextManipulator/static/js/2.7c1add86.chunk.js"
+  },
+  {
+    "revision": "5b5e295b61354899e591",
+    "url": "/TextManipulator/static/js/runtime~main.5b5e295b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextManipulator/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "79104fcf590ca9083aa727123f6509dd",
+    "url": "/TextManipulator/index.html"
+  }
+];
